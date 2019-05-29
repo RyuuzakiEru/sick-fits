fe2010a32c145135fbe7ebfc326b1a2ca47d48e8
RyuuzakiEru <rmorales82@outlook.com>
2019-05-29 14:30:44 +0200

    Refactor Cart using react-adopt added frontend eslint config added prettier global config

--- frontend/components/Cart.js
+++ frontend/components/Cart.js
@@ -1,14 +1,15 @@
-import React from "react";
-import { Query, Mutation } from "react-apollo";
-import gql from "graphql-tag";
-import User from "./User";
-import Supreme from "./styles/Supreme";
-import CartStyles from "./styles/CartStyles";
-import CloseButton from "./styles/CloseButton";
-import SickButton from "./styles/SickButton";
+import React from 'react';
+import { Query, Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
+import { adopt } from 'react-adopt';
+import User from './User';
+import Supreme from './styles/Supreme';
+import CartStyles from './styles/CartStyles';
+import CloseButton from './styles/CloseButton';
+import SickButton from './styles/SickButton';
 import CartItem from './CartItem';
 import calcTotalPrice from '../lib/calcTotalPrice';
-import formatMoney from "../lib/formatMoney";
+import formatMoney from '../lib/formatMoney';
 
 const LOCAL_STATE_QUERY = gql`
   query {
@@ -22,42 +23,46 @@ const TOGGLE_CART_MUTATION = gql`
   }
 `;
 
-const Cart = () => {
-  return (
-    <User>
-      {({ data: { me } }) => {
-        if (!me) return null;
-        return (
-          <Mutation mutation={TOGGLE_CART_MUTATION}>
-            {toggleCart => (
-              <Query query={LOCAL_STATE_QUERY}>
-                {({ data }) => (
-                  <CartStyles open={data.cartOpen}>
-                    <header>
-                      <CloseButton title="Close" onClick={toggleCart}>
-                        &times;
-                      </CloseButton>
-                      <Supreme>{me.name} Cart</Supreme>
-                      <p>You have {me.cart.length} Item{me.cart.length ===1 ? '':'s'}</p>
-                    </header>
-                    <ul>
-                      {me.cart.map(cartItem =><CartItem cartItem={cartItem} key={cartItem.id} />)}
-                    </ul>
+const Composed = adopt({
+  user: ({ render }) => <User>{render}</User>,
+  toggleCart: ({ render }) => (
+    <Mutation mutation={TOGGLE_CART_MUTATION}>{render}</Mutation>
+  ),
+  localState: ({ render }) => <Query query={LOCAL_STATE_QUERY}>{render}</Query>
+});
 
-                    <footer>
-                      <p>{formatMoney(calcTotalPrice(me.cart))}</p>
-                      <SickButton>Checkout</SickButton>
-                    </footer>
-                  </CartStyles>
-                )}
-              </Query>
-            )}
-          </Mutation>
-        );
-      }}
-    </User>
-  );
-};
+const Cart = () => (
+  <Composed>
+    {({ user, toggleCart, localState }) => {
+      const { me } = user.data;
+      if (!me) return null;
+      return (
+        <CartStyles open={localState.data.cartOpen}>
+          <header>
+            <CloseButton title="Close" onClick={toggleCart}>
+              &times;
+            </CloseButton>
+            <Supreme>{me.name} Cart</Supreme>
+            <p>
+              You have {me.cart.length} Item
+              {me.cart.length === 1 ? '' : 's'}
+            </p>
+          </header>
+          <ul>
+            {me.cart.map(cartItem => (
+              <CartItem cartItem={cartItem} key={cartItem.id} />
+            ))}
+          </ul>
+
+          <footer>
+            <p>{formatMoney(calcTotalPrice(me.cart))}</p>
+            <SickButton>Checkout</SickButton>
+          </footer>
+        </CartStyles>
+      );
+    }}
+  </Composed>
+);
 export { LOCAL_STATE_QUERY };
 export { TOGGLE_CART_MUTATION };
 export default Cart;
