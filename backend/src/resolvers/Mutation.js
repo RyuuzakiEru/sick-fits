31b11e6c96bda6333e6c291a5bea83a8135eb765
RyuuzakiEru <rmorales82@outlook.com>
2019-06-01 09:23:15 +0200

    Stripe Checkout / Backend part 1

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -4,6 +4,7 @@ const {randomBytes} = require('crypto');
 const {promisify} = require('util');
 const {transport, makeANiceEmail} = require('../mail');
 const {hasPermission} = require('../utils');
+const stripe = require('../stripe');
 
 const Mutations = {
   async createItem(parent, args, ctx, info) {
@@ -12,18 +13,18 @@ const Mutations = {
     }
 
     const item = await ctx.db.mutation.createItem(
-        {
-          data: {
-            // This is how to create a relationship between the Item and the User
-            user: {
-              connect: {
-                id: ctx.request.userId,
-              },
-            },
-            ...args,
-          },
-        },
-        info
+		{
+			data: {
+				// This is how to create a relationship between the Item and the User
+				user: {
+					connect: {
+						id: ctx.request.userId
+					}
+				},
+				...args
+			}
+		},
+		info
     );
 
     console.log(item);
@@ -37,23 +38,23 @@ const Mutations = {
     delete updates.id;
     // run the update method
     return ctx.db.mutation.updateItem(
-        {
-          data: updates,
-          where: {
-            id: args.id,
-          },
-        },
-        info
+		{
+			data: updates,
+			where: {
+				id: args.id
+			}
+		},
+		info
     );
   },
   async deleteItem(parent, args, ctx, info) {
     const where = { id: args.id };
     // 1. find the item
-    const item = await ctx.db.query.item({where}, `{ id title user { id }}`);
+	  const item = await ctx.db.query.item({where}, `{ id title user { id }}`);
     // 2. Check if they own that item, or have the permissions
     const ownsItem = item.user.id === ctx.request.userId;
     const hasPermissions = ctx.request.user.permissions.some(permission =>
-        ['ADMIN', 'ITEMDELETE'].includes(permission)
+		['ADMIN', 'ITEMDELETE'].includes(permission)
     );
 
     if (!ownsItem && !hasPermissions) {
@@ -70,21 +71,21 @@ const Mutations = {
     const password = await bcrypt.hash(args.password, 10);
     // create the user in the database
     const user = await ctx.db.mutation.createUser(
-        {
-          data: {
-            ...args,
-            password,
-            permissions: {set: ['USER']},
-          },
-        },
-        info
+		{
+			data: {
+				...args,
+				password,
+				permissions: {set: ['USER']}
+			}
+		},
+		info
     );
     // create the JWT token for them
     const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
     // We set the jwt as a cookie on the response
     ctx.response.cookie('token', token, {
       httpOnly: true,
-      maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie
+		maxAge: 1000 * 60 * 60 * 24 * 365 // 1 year cookie
     });
     // Finalllllly we return the user to the browser
     return user;
@@ -105,18 +106,18 @@ const Mutations = {
     // 4. Set the cookie with the token
     ctx.response.cookie('token', token, {
       httpOnly: true,
-      maxAge: 1000 * 60 * 60 * 24 * 365,
+		maxAge: 1000 * 60 * 60 * 24 * 365
     });
     // 5. Return the user
     return user;
   },
   signout(parent, args, ctx, info) {
     ctx.response.clearCookie('token');
-    return {message: 'Goodbye!'};
+	  return {message: 'Goodbye!'};
   },
   async requestReset(parent, args, ctx, info) {
     // 1. Check if this is a real user
-    const user = await ctx.db.query.user({where: {email: args.email}});
+	  const user = await ctx.db.query.user({where: {email: args.email}});
     if (!user) {
       throw new Error(`No such user found for email ${args.email}`);
     }
@@ -125,8 +126,8 @@ const Mutations = {
     const resetToken = (await randomBytesPromiseified(20)).toString('hex');
     const resetTokenExpiry = Date.now() + 3600000; // 1 hour from now
     const res = await ctx.db.mutation.updateUser({
-      where: {email: args.email},
-      data: {resetToken, resetTokenExpiry},
+		where: {email: args.email},
+		data: {resetToken, resetTokenExpiry}
     });
     // 3. Email them that reset token
     const mailRes = await transport.sendMail({
@@ -135,12 +136,13 @@ const Mutations = {
       subject: 'Your Password Reset Token',
       html: makeANiceEmail(`Your Password Reset Token is here!
       \n\n
-      <a href="${process.env
-          .FRONTEND_URL}/reset?resetToken=${resetToken}">Click Here to Reset</a>`),
+      <a href="${
+		  process.env.FRONTEND_URL
+		  }/reset?resetToken=${resetToken}">Click Here to Reset</a>`)
     });
 
     // 4. Return the message
-    return {message: 'Thanks!'};
+	  return {message: 'Thanks!'};
   },
   async resetPassword(parent, args, ctx, info) {
     // 1. check if the passwords match
@@ -152,8 +154,8 @@ const Mutations = {
     const [user] = await ctx.db.query.users({
       where: {
         resetToken: args.resetToken,
-        resetTokenExpiry_gte: Date.now() - 3600000,
-      },
+		  resetTokenExpiry_gte: Date.now() - 3600000
+	  }
     });
     if (!user) {
       throw new Error('This token is either invalid or expired!');
@@ -162,19 +164,19 @@ const Mutations = {
     const password = await bcrypt.hash(args.password, 10);
     // 5. Save the new password to the user and remove old resetToken fields
     const updatedUser = await ctx.db.mutation.updateUser({
-      where: {email: user.email},
+		where: {email: user.email},
       data: {
         password,
         resetToken: null,
-        resetTokenExpiry: null,
-      },
+		  resetTokenExpiry: null
+	  }
     });
     // 6. Generate JWT
     const token = jwt.sign({ userId: updatedUser.id }, process.env.APP_SECRET);
     // 7. Set the JWT cookie
     ctx.response.cookie('token', token, {
       httpOnly: true,
-      maxAge: 1000 * 60 * 60 * 24 * 365,
+		maxAge: 1000 * 60 * 60 * 24 * 365
     });
     // 8. return the new user
     return updatedUser;
@@ -186,28 +188,28 @@ const Mutations = {
     }
     // 2. Query the current user
     const currentUser = await ctx.db.query.user(
-        {
-          where: {
-            id: ctx.request.userId,
-          },
-        },
-        info
+		{
+			where: {
+				id: ctx.request.userId
+			}
+		},
+		info
     );
     // 3. Check if they have permissions to do this
     hasPermission(currentUser, ['ADMIN', 'PERMISSIONUPDATE']);
     // 4. Update the permissions
     return ctx.db.mutation.updateUser(
-        {
-          data: {
-            permissions: {
-              set: args.permissions,
-            },
-          },
-          where: {
-            id: args.userId,
-          },
-        },
-        info
+		{
+			data: {
+				permissions: {
+					set: args.permissions
+				}
+			},
+			where: {
+				id: args.userId
+			}
+		},
+		info
     );
   },
   async addToCart(parent, args, ctx, info) {
@@ -220,44 +222,44 @@ const Mutations = {
     const [existingCartItem] = await ctx.db.query.cartItems({
       where: {
         user: { id: userId },
-        item: {id: args.id},
-      },
+		  item: {id: args.id}
+	  }
     });
     // 3. Check if that item is already in their cart and increment by 1 if it is
     if (existingCartItem) {
       console.log('This item is already in their cart');
       return ctx.db.mutation.updateCartItem(
-          {
-            where: {id: existingCartItem.id},
-            data: {quantity: existingCartItem.quantity + 1},
-          },
-          info
+		  {
+			  where: {id: existingCartItem.id},
+			  data: {quantity: existingCartItem.quantity + 1}
+		  },
+		  info
       );
     }
     // 4. If its not, create a fresh CartItem for that user!
     return ctx.db.mutation.createCartItem(
-        {
-          data: {
-            user: {
-              connect: {id: userId},
-            },
-            item: {
-              connect: {id: args.id},
-            },
-          },
-        },
-        info
+		{
+			data: {
+				user: {
+					connect: {id: userId}
+				},
+				item: {
+					connect: {id: args.id}
+				}
+			}
+		},
+		info
     );
   },
   async removeFromCart(parent, args, ctx, info) {
     // 1. Find the cart item
     const cartItem = await ctx.db.query.cartItem(
-        {
-          where: {
-            id: args.id,
-          },
-        },
-        `{ id, user { id }}`
+		{
+			where: {
+				id: args.id
+			}
+		},
+		`{ id, user { id }}`
     );
     // 1.5 Make sure we found an item
     if (!cartItem) throw new Error('No CartItem Found!');
@@ -267,12 +269,56 @@ const Mutations = {
     }
     // 3. Delete that cart item
     return ctx.db.mutation.deleteCartItem(
-        {
-          where: {id: args.id},
-        },
-        info
+		{
+			where: {id: args.id}
+		},
+		info
     );
   },
+
+	async createOrder(parent, args, ctx, info) {
+		// query user and check logged in
+		const {userId} = ctx.request;
+		if (!userId) throw new Error('You must be signed in to complete order');
+		// fetch current user and cart content
+		const user = await ctx.db.query.user(
+			{where: {id: userId}},
+			`{
+        id
+        name
+        email
+        cart
+        {
+          id
+          quantity
+          item
+          {
+            title
+            price
+            id
+            description
+            image
+          }
+        }
+      }`
+		);
+		// recalculate price to prevent UI price tampering
+
+		const amount = user.cart.reduce(
+			(tally, cartItem) => tally + (cartItem.item.price * cartItem.quantity), 0
+		);
+		//console.log(amount);
+		// Create Stripe Charge (turn token into money)
+		const charge = await stripe.charges.create({
+			amount,
+			currency: 'USD',
+			source: args.token,
+		});
+		// TODO convert cartitems to orderItems
+		// TODO create order
+		// TODO clean up cart - delete cartitems
+		// TODO return order to client
+	}
 };
 
 module.exports = Mutations;
--- frontend/components/TakeMyMoney.js
+++ frontend/components/TakeMyMoney.js
@@ -1,41 +1,61 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Stripecheckout from 'react-stripe-checkout';
-import { Mutation } from 'react-apollo';
-import Router from 'next/router';
-import NProgress from 'nprogress';
-import PropTypes from 'prop-types';
+import {Mutation} from 'react-apollo';
 import gql from 'graphql-tag';
 import calcTotalPrice from '../lib/calcTotalPrice';
-import Error from './ErrorMessage';
-import User, { CURRENT_USER_QUERY } from './User';
+import User, {CURRENT_USER_QUERY} from './User';
+
+const CREATE_ORDER_MUTATION = gql`
+  mutation createOrder($token: String!) {
+    createOrder(token: $token) {
+      id
+      charge
+      total
+      items {
+        id
+        title
+      }
+    }
+  }
+`;
 
 function totalItems(cart) {
   return cart.reduce((tally, cartItem) => tally + cartItem.quantity, 0);
 }
 
 class TakeMyMoney extends Component {
-  onToken= (res) => {
-    console.log('Token res');
+  onToken = (res, createOrder) => {
+    //console.log(res.id);
+    createOrder({
+      variables: {
+        token: res.id
+      }
+    }).catch(err => alert(err.message));
   };
-
 
   render() {
     return (
       <User>
         {({ data: { me } }) => (
-          <Stripecheckout
-            amount={calcTotalPrice(me.cart)}
-            name="Sick-fits"
-            description={`Order of ${totalItems(me.cart)} items}`}
-            image={me.cart[0].item && me.cart[0].item.image}
-            stripeKey='pk_test_RuvPNz4fo12IMYdL4xqndqVL00F5ttg16p'
-            currency='USD'
-            email={me.email}
-            token={res => this.onToken(res)}
-
-          >
-            {this.props.children}
-          </Stripecheckout>
+            <Mutation
+                mutation={CREATE_ORDER_MUTATION}
+                refetchQueries={[{query: CURRENT_USER_QUERY}]}
+            >
+              {createOrder => (
+                  <Stripecheckout
+                      amount={calcTotalPrice(me.cart)}
+                      name="Sick-fits"
+                      description={`Order of ${totalItems(me.cart)} items}`}
+                      image={me.cart[0].item && me.cart[0].item.image}
+                      stripeKey="pk_test_RuvPNz4fo12IMYdL4xqndqVL00F5ttg16p"
+                      currency="USD"
+                      email={me.email}
+                      token={res => this.onToken(res, createOrder)}
+                  >
+                    {this.props.children}
+                  </Stripecheckout>
+              )}
+            </Mutation>
         )}
       </User>
     );
